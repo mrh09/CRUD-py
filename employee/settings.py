c5478ce75028c3ecec75275863fbcb68a5f073fd
mrh-develop <dayatmrh@gmail.com>
2021-04-06 20:21:58 +0700

    make it better

--- employee/settings.py
+++ employee/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'employee.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates/base.html')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates/login.html')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
